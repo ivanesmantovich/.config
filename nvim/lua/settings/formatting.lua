d0740a788a46804d99c3362bbb7b8bd905e98c66
Ivan Esmantovich <ivanesmantovich@gmail.com>
2024-09-09 12:54:07 +0300

    conform.nvim

--- nvim/lua/settings/formatting.lua
+++ nvim/lua/settings/formatting.lua
@@ -1,14 +1,34 @@
--- TODO: Захардкодить пути к проектам в которых должен работать форматтер
-
--- require("conform").setup({
---   formatters_by_ft = {
---     javascript = { "biome" }
---   },
--- })
-
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*",
---   callback = function(args)
---     require("conform").format({ bufnr = args.buf })
---   end,
--- })
+local conform_util = require('conform.util')
+require("conform").setup({
+  -- NOTE: Если будет странное поведение, то попробовать format_on_save без async'а
+  format_after_save = {
+    async = true
+  },
+  notify_on_error = false,
+  notify_no_formatters = false,
+  formatters_by_ft = {
+    javascript = { 'biome', 'prettier' },
+    javascriptreact = { 'biome', 'prettier' },
+    typescript = { 'biome', 'prettier' },
+    typescriptreact = { 'biome', 'prettier' }
+  },
+  formatters = {
+    prettier = {
+      require_cwd = true,
+      cwd = conform_util.root_file({
+        ".prettierrc", 
+        ".prettierrc.json", 
+        ".prettierrc.yml", 
+        ".prettierrc.yaml", 
+        ".prettierrc.json5", 
+        ".prettierrc.js", 
+        ".prettierrc.cjs", 
+        ".prettierrc.mjs", 
+        ".prettierrc.toml", 
+        "prettier.config.js", 
+        "prettier.config.cjs", 
+        "prettier.config.mjs",
+      })
+    }
+  }
+})
